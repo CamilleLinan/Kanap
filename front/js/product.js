0ae54ea2c238fbf16327c07dea8f0cfa0dfeab07
Camille Linan <camille.linan@hotmail.com>
2022-07-06 23:30:41 +0200

    add-product-page

--- front/js/product.js
+++ front/js/product.js
@@ -1,65 +1,41 @@
-// JS de la page détail: product.html
-
 // 1/ recuperer l'id dans l'url
 
-const str = window.location.search;
+const qStr = window.location.search;
 
-const urlParams = new URLSearchParams(str);
+const urlParams = new URLSearchParams(qStr);
 
 const id = urlParams.get('id');
-console.log (id);
 
 const apiUrl = 'http://localhost:3000/api/products/' + id;
 
 // 2/ si il y a un id, alors on va appeler le back/l'api pour chercher les informations du produit d'id
 
-let cardsFetch = function () {
-    fetch(apiUrl)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
+fetch(apiUrl)
+    .then((response) => response.json()
+    .then((data) => {
             
-            // Afficher les images
-            let img = document.querySelector('.item__img');
-            img.innerHTML = `<img src='${data.imageUrl}' alt='${data.altTxt}'>`;
+        // Afficher les images
+        let img = document.querySelector('.item__img');
+        img.innerHTML = `<img src='${data.imageUrl}' alt='${data.altTxt}'>`;
             
-            // Afficher les noms
-            let name = document.querySelector('#title');
-            name.innerHTML = `${data.name}`;
+        // Afficher les noms
+        let name = document.querySelector('#title');
+        name.innerHTML = `${data.name}`;
             
-            // Afficher les prix
-            let price = document.querySelector('#price');
-            price.innerHTML = `${data.price}`;
+        // Afficher les prix
+        let price = document.querySelector('#price');
+        price.innerHTML = `${data.price}`;
 
-            // Afficher les descriptions
-            let desc = document.querySelector('#description');
-            desc.innerHTML = `${data.description}`;
+        // Afficher les descriptions
+        let desc = document.querySelector('#description');
+        desc.innerHTML = `${data.description}`;
 
-            // Afficher les couleurs
-            let color = document.querySelector('#colors');
-            for (i = 0; i < data.colors.length; i++) {
-                color.innerHTML += `<option value='${data.colors[i]}'>${data.colors[i]}</option>`;
-            }
-        });
-};
-
-cardsFetch();
-/*
-
-fetch(url)
-    .then(response => {
-        si il y a un produit dans la reponse
-        alors j'affiche le nom, l'image, etc...
-
-        sinon
-        affiche un message
-    })
-
-*/
-
-// si pas d'id alors affiche un message
-/* 
-
-    .catch((err) => document.querySelector('#items').innerText = `Oups ! Il y a eu une erreur lors de l'affichage du produit :(`);
-
-*/
+        // Afficher les couleurs
+        let color = document.querySelector('#colors');
+        for (i = 0; i < data.colors.length; i++) {
+            color.innerHTML += `<option value='${data.colors[i]}'>${data.colors[i]}</option>`;
+        }
+    }))
+    
+    .catch((err) => 
+        document.querySelector('.item').innerText = `Oups ! Il y a eu une erreur lors de l'affichage du produit ! :(`);
