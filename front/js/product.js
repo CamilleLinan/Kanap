464222fa0d2ae4c53156703f03bed794ed73afb2
Camille Linan <camille.linan@hotmail.com>
2022-07-18 00:17:08 +0200

    add-product-to-localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -31,7 +31,7 @@ fetch(apiUrl)
         desc.innerHTML = `${data.description}`;
 
         // Afficher les couleurs
-        let color = document.querySelector('#colors');
+        let color = document.querySelector(`#colors`);
         for (i = 0; i < data.colors.length; i++) {
             color.innerHTML += `<option value='${data.colors[i]}'>${data.colors[i]}</option>`;
         }
@@ -44,20 +44,62 @@ fetch(apiUrl)
 
 // Récuperer la couleur choisie
 function colorValue() {
-    let color = document.querySelector('#colors');
+    let color = document.querySelector(`#colors`);
     return color.value;
 };
 
 // Récuperer la quantité choisie
 function qtyValue() {
-    let qty = document.querySelector('#quantity');
+    let qty = document.querySelector(`#quantity`);
     return qty.value;
 };
 
 // Bouton d'ajout au panier
-const addToCart = document.querySelector('#addToCart');
+const addToCart = document.querySelector(`#addToCart`);
 
-addToCart.addEventListener('click', () => {
+// Lors du 'click' on écoute la couleur et la quantité du produit sélectionné
+addToCart.addEventListener(`click`, () => {
     let color = colorValue();
     let qty = parseInt(qtyValue());
+
+// Créer un objet de la sélection utlisateur avec l'id du produit
+    const selectionUser = {
+        colorChosen: color,
+        qtyChosen: qty,
+        productChosen: id,
+    };
+
+// Récupérer le panier dans le localStorage
+    let cartInLocalStorage = JSON.parse(localStorage.getItem(`selectedProduct`));
+    
+    // Fonction pop up
+    const popupConfirm = () => {
+        if(window.confirm(`Le produit a bien été ajouté au panier.
+Consulter le panier OK ou revenir à l'accueil ANNULER`)) {  
+            window.location.href = "./cart.html";
+        } else {
+            window.location.href = "./index.html";
+        }
+    }
+
+    // Fonction ajouté un produit dans le localStorage
+    const addProductLocalStorage = () => {
+        cartInLocalStorage.push(selectionUser);
+        localStorage.setItem(`selectedProduct`, JSON.stringify(cartInLocalStorage));
+    };
+
+    // Si le panier existe dans le localStorage
+    if (cartInLocalStorage) {
+        console.log(`panier existe`);
+        addProductLocalStorage();
+        popupConfirm();
+    }
+
+    // Sinon
+    else {
+        console.log(`panier n'existe pas`);
+        cartInLocalStorage = [];
+        addProductLocalStorage();
+        popupConfirm();
+    }
 });
